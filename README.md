16edfa89fb6074bf524f1d3552b44505f0e3d999
Vladislav Pavlenko <pavlenkoovd@gmail.com>
2022-11-03 00:10:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task "People"
+## Practice with the Builder template (Builder). 
+Design a Person class that stores person's data.
---
+++ src/PersonBuilder.java
@@ -0,0 +1,41 @@
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Неправильно указан возраст");
+        } else {
+            this.age = age;
+            return this;
+        }
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Не задано имя и/или фамилия человека");
+        }
+        Person person = new Person(this.name, this.surname);
+        person.age = this.age;
+        person.address = this.address;
+
+        return person;
+    }
+}
